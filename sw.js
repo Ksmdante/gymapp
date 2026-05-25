e7c57dbd5a8ca394abfcb062e848cc582c155e82
ksmdante <drewfulton668@gmail.com>
2026-05-25 12:08:35 +1000

    Bump SW cache to v2, add sheets.js to precache list

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gymapp-v1';
+const CACHE_NAME = 'gymapp-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -6,7 +6,8 @@ const ASSETS = [
   './css/styles.css',
   './js/app.js',
   './js/workouts.js',
-  './js/storage.js'
+  './js/storage.js',
+  './js/sheets.js'
 ];
 
 self.addEventListener('install', (e) => {
